a696e0d07822d4524c672c7e06610fdbc12f5c1a
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 00:44:52 +0100

    modifying code file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,3 @@
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    subscrib_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "Med"},
-                            allow_redirects=False)
-    if subscrib_info.status_code >= 300:
-        return 0
-
-    return subscrib_info.json().get("data").get("subscribers")
