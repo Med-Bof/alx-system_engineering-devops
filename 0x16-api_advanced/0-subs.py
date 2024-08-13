0c735ef9ce481237b30f08779bafe7bed730e3f2
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 00:34:06 +0100

    nodifying file 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
+    """Returns the number of subscribers
     to the subreddit"""
     import requests
 
