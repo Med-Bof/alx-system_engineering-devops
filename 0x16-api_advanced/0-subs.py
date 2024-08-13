63152c5ad9b1a6b445a72fadb41dbfb0cf73cba5
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 00:22:30 +0100

    adding file 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import requests
 
-def get_subscriber_count(subreddit_name):
-    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit_name)
+def number_of_subscribers(subreddit):
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     request_headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -10,4 +10,4 @@ def get_subscriber_count(subreddit_name):
     if response.status_code == 404:
         return 0
     response_data = response.json().get("data")
-    return response_data.get("subscribers")
+    return response_data.get("subscribers", 0)
