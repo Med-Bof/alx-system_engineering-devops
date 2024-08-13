85b5a527eb587324c736dfec22cc3beff9b660a8
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 00:47:31 +0100

    modifying code file

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+""" Task 100"""
+
+def count_words(subreddit, word_list, after="", word_dic={}):
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Task 2"""
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
