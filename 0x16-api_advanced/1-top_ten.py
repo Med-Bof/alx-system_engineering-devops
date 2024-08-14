2ef49877f5b89b6c3c8f56ecd65295dd4edb39c6
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 20:57:25 +0100

    modifying code file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 """Task 1"""
+import requests
 
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "python:subreddit.top.ten:v1.0.0 (by /u/Med-Bof)"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            posts = response.json().get("data", {}).get("children", [])
+            for post in posts:
+                print(post.get("data", {}).get("title"))
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
