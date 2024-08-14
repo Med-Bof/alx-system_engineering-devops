f47607f27956b20ca0a9544be5781bb94cf75556
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 20:50:07 +0100

    modifying code file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 """task 0"""
 
+import requests
 
 def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0.0 (by /u/your_username)"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data")
+            return data.get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
