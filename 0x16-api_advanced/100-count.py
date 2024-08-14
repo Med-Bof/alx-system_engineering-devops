e3f23cf55bffdedd96aac8682464a31b52a11983
Med-Bof <mohammedboaafia@gmail.com>
2024-08-14 21:11:46 +0100

    modifying code file

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,7 +9,7 @@ def count_words(subreddit, word_list, after="", counts=None):
         counts = {}
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "python:subreddit.keyword.counter:v1.0.0 (by /u/your_username)"}
+    headers = {"User-Agent": "python:subreddit.keyword.counter:v1.0.0 (by /u/Med-Bof)"}
     params = {"after": after, "limit": 100}
 
     try:
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python3
 """Task 2"""
 
+import requests
 
-def recurse(subreddit, hot_list=[], count=0, after=None):
+def recurse(subreddit, hot_list=[], after=None):
+
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "python:subreddit.recurse:v1.0.0 (by /u/your_username)"}
+    params = {"limit": 100, "after": after}
+    
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        if response.status_code != 200:
+            return None
+        
+        data = response.json().get("data", {})
+        after = data.get("after")
+        posts = data.get("children", [])
+
+        for post in posts:
+            hot_list.append(post.get("data", {}).get("title"))
+
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list if hot_list else None
+    
+    except requests.RequestException:
+        return None
+
